960dab892262bb4b5dc465090348ead01ce24ebf
TropicalShadow <me@tesseract.club>
2025-12-27 19:11:14 +0000

    feat: bump java to 25, gradle 9.2.1, minestom: 1.21.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
-plugins {
+ plugins {
     id("smolder.publishing-conventions")
 }
 
 dependencies {
-    compileOnly("net.minestom:minestom:2025.09.13-1.21.8")
+    compileOnly("net.minestom:minestom:2025.12.20-1.21.11")
     api("org.slf4j:slf4j-api:2.0.16")
     api("com.github.luben:zstd-jni:1.5.7-4")
 }
+
+ java {
+     toolchain {
+         languageVersion.set(JavaLanguageVersion.of(25)) // Minestom has a minimum Java version of 25
+     }
+ }
